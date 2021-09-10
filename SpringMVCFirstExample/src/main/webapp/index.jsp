fe8012f538aeacb18e67d52e8c3c8e3c448e1cb6
perlajavadevops <perladevops@gmail.com>
2021-09-10 23:25:22 +0530

    Added Spring MVC examples

---
+++ SpringMVCFirstExample/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+	<p>Welcome to Spring MVC First Demo</p>
+</body>
+</html>
---
+++ SpringMVCLoginDemo/src/main/java/com/pgr/mvc/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.pgr.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.pgr.mvc.bean.LoginBean;
+
+@Controller
+public class LoginController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String init(Model model) {
+		model.addAttribute("msg", "Please Enter Your Login Details");
+		return "login";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String submit(Model model, @ModelAttribute("loginBean") LoginBean loginBean) {
+
+		if (loginBean != null) {
+			if (loginBean.getUserName().equals("perlareddy")) {
+				model.addAttribute("msg", loginBean.getUserName());
+				return "success";
+			} else {
+				model.addAttribute("error", "Invalid Details");
+				return "login";
+			}
+		} else {
+			model.addAttribute("error", "Please enter Details");
+			return "login";
+		}
+	}
+}
